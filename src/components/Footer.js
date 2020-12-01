468c8d7590b43f2100c722527bf5227dd08e0d29
Will-Peterson <william.from.ca@gmail.com>
2020-12-01 02:09:03 -0800

    refactor: updated Footer date
    changed plain text year to dynamic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <a href='#about'> About </a>|
             <a href='#projects'> Projects </a>|
             <a href='#contact'> Contact</a>
-            <span>&copy; 2020 <a href='www.williamrpeterson.com'>williamrpeterson.com</a></span>
+            <span>&copy; {new Date().getFullYear()} <a href='www.williamrpeterson.com'>williamrpeterson.com</a></span>
         </div>
     );
 }
